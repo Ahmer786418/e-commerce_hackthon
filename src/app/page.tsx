4bf223b89f7e781f531214cc2e69c29288b84cdc
Ahmer786418 <ahmerabbas400@gmail.com>
2024-12-09 23:06:15 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+import Home from "./components/home";
+// import Footer from "./components/footer";
+
+
+
+
+export default function Hoome() {
+  return (
+
+    <><div>
+
+
+      <Home />
+
+
+
+    </div>
+
+      </>
+  );
+}
